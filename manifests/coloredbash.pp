3bd10fe17bcce72da75e89f180ee3b3f62f8ba9f
Alexander Fortin <alexander.fortin@kaufda.de>
2013-09-13 17:07:39 +0200

    Add bash links for root

--- manifests/coloredbash.pp
+++ manifests/coloredbash.pp
@@ -1,6 +1,17 @@
+#
+# Update force_color_prompt and links bashrc for root
+#
 class utils::coloredbash () {
   exec { 'update_bashrc':
     command => '/bin/echo force_color_prompt=yes >> /etc/bash.bashrc',
     unless  => '/bin/grep "^force_color_prompt=yes$" /etc/bash.bashrc',
+  }
+
+  $dotlinks = ['bashrc', 'profile', 'bash_logout']
+  $dotlinks.foreach { |$x|
+    file { "/root/.${x}":
+      ensure => link,
+      target => "/etc/skel/.${x}",
+    }
   }
 }
