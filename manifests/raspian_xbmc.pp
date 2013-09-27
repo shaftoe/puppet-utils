3261191d9b4806cc2012c2fbe28de405f1e2ef5f
Alexander Fortin <alexander.fortin@kaufda.de>
2013-09-27 18:26:03 +0200

    Add utils::raspian_xbmc class

---
+++ manifests/raspian_xbmc.pp
@@ -0,0 +1,23 @@
+class utils::raspian_xbmc ($ensure = present) {
+
+  require 'utils::users'
+
+  apt::source { 'raspian_xbmc':
+    location          => 'http://archive.mene.za.net/raspbian/',
+    release           => 'wheezy',
+    repos             => 'contrib',
+    key               => '5243CDED',
+    key_server        => 'keyserver.ubuntu.com',
+    pin               => '-10',
+    include_src       => true
+  }
+
+  package { 'xbmc':
+    ensure  => $ensure,
+    require => [
+      Apt::Source['raspian_xbmc'],
+      User['xbmc'],
+    ],
+  }
+
+}
