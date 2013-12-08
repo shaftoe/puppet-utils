908c18297ab60abf4b9b077bc893309685fec5b0
Alexander Fortin <alexander.fortin@gmail.com>
2013-12-08 17:36:20 +0100

    Add .profile for CentOS

--- manifests/users.pp
+++ manifests/users.pp
@@ -1,4 +1,14 @@
 class utils::users () {
   $users = hiera_hash('users')
   $users.foreach { |$x| utils::user{ $x[0]: }}
+
+  if $::osfamily == 'RedHat' {
+    file {'/etc/skel/.profile':
+      ensure  => present,
+      owner   => 'root',
+      mode    => '0444',
+      content => 'puppet:///modules/utils/profile',
+    }
+  }
+
 }
