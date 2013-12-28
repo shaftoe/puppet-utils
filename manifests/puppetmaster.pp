e66c240197e6225d4e3c5e57530a375c0b114558
Alexander Fortin <alexander.fortin@gmail.com>
2013-12-28 12:33:08 +0100

    Add ensure to utils::puppetmaster

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,5 +1,6 @@
 # Setup sync cronjobs for puppetmaster
 class utils::puppetmaster (
+  $ensure            = present,
   $puppetpath        = '/etc/puppet',
   $manifestsbasepath = '/etc/puppet',
   $hierabasepath     = '/var/lib/hiera'
@@ -7,24 +8,31 @@ class utils::puppetmaster (
 
   include 'utils::puppetagent'
 
-  tidy { '/var/lib/puppet/reports':
-    recurse => true,
-    age     => '1w',
+  if $ensure == present {
+
+    tidy { '/var/lib/puppet/reports':
+      recurse => true,
+      age     => '1w',
+    }
+
   }
 
   cron { 'puppet-repo-update':
+    ensure  => $ensure,
     user    => 'root',
     command => "cd ${manifestsbasepath} && git pull &> /dev/null",
     minute  => '*/5'
   }
 
   cron { 'hiera-repo-update':
+    ensure  => $ensure,
     user    => 'root',
     command => "cd ${hierabasepath} && git pull > /dev/null 2> /dev/null",
     minute  => '*/5',
   }
 
   cron { 'librarian-update':
+    ensure      => $ensure,
     environment => 'PATH=/bin:/usr/bin:/usr/sbin:/usr/local/bin',
     user        => 'root',
     command     => "cd ${puppetpath} && librarian-puppet install",
